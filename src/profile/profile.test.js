1b2005e6bc0531ac5d786bd0dec43470665af7d1
Florian Lenz <flenz81@googlemail.com>
2017-11-03 23:24:30 +0100

    [tests] profile - added enhanced tests for profile

--- src/profile/profile.test.js
+++ src/profile/profile.test.js
@@ -1,15 +1,99 @@
 const profile = require('./profile');
 const errors = require('./../../lib/errors');
+const { spawn } = require('child_process');
 
 describe('profile', () => {
     "use strict";
 
-    /**
-     * A profile has three parameters, a pseudo name, a description and a image
-     */
-    test('setProfile', () => {
+    describe('setProfile', () => {
 
-        return expect(profile.setProfile('pseudoName', 'I am a florian', 'base64...')).resvoles.toBeUndefined();
+        /**
+         * A profile has three parameters, a pseudo name, a description and a image
+         * This is a functional test
+         */
+        test('create profile', () => {
+
+            // Kill the database
+            spawn.execSync('npm run db:flush');
+
+            return expect(new Promise((res, rej) => {
+
+                profile
+                    .setProfile('pseudoName', 'I am a florian', 'base64...')
+                    .then(result => {
+                        return profile.getProfile();
+                    })
+                    .then(profile => {
+                        res(profile);
+                    })
+                    .catch(err => {
+                        rej(err);
+                    })
+
+            }))
+                .resvoles
+                .toEqual({
+                    pseudo: 'pseudoName',
+                    description: 'I am a florian',
+                    image: 'base64...'
+                })
+
+        });
+
+        test('try to update profile', () => {
+
+            // Kill the database
+            spawn.execSync('npm run db:flush');
+
+            const profilePromise = profile
+
+                //Create profile
+                .setProfile('pseudoName', 'I am a florian', 'base64...')
+
+                //After saving fetch it and return the promise
+                .then(_ => {
+
+                    return profile.getProfile();
+
+                })
+
+                //The profile content should match since get profile
+                //fetched the profile
+                .then(profileContent => {
+
+                    //Assert profile matches
+                    expect(profileContent).toEqual({
+                        pseudo: 'pseudoName',
+                        description: 'I am a florian',
+                        image: 'base64...'
+                    });
+
+                    //Update profile after ensure that it was written to the db
+                    return profile
+                        .setProfile({
+                            pseudo: 'pseudoNameUpdated',
+                            description: 'I am a florian',
+                            image: 'base64...'
+                        });
+
+                })
+
+                // Fetch the updated profile
+                .then(_ => {
+
+                    return profile.getProfile()
+
+                });
+
+            return expect(profilePromise)
+                .resvoles
+                .toEqual({
+                    pseudo: 'pseudoNameUpdated',
+                    description: 'I am a florian',
+                    image: 'base64...'
+                });
+
+        })
 
     });
 
@@ -40,7 +124,7 @@ describe('profile', () => {
 
             return expect(profile.getProfile())
                 .rejects
-                .toBe(new errors.NoProfilePresent())
+                .toEqual(new errors.NoProfilePresent())
 
         });
 
@@ -69,18 +153,26 @@ describe('profile', () => {
             return expect(profile.getPublicProfile())
                 .resolves
                 .toEqual({
-                    pseudo: '',
-                    description: '',
+                    pseudo: 'peasded',
+                    description: 'I am a description',
                     image: 'base64....',
+
+                    //Public eth addresses
                     ethAddresses: [
                         "0x2a65aca4d5fc5b5c859090a6c34d164135398226"
                     ],
+
+                    //Mesh network keys (p2p lib identity)
                     meshKeys: [
                         "QmUKydZyhZmt2x5VpLJSojRarhRrC4k9QYpkYNf23sWy98"
                     ],
-                    ids : [
+
+                    //Rsa key's for signing messages
+                    identKey : [
                         "QmczUvgj46cvf4wWv8y3Z7RFiKyTUGSx3cSdL4Tqo5aevT"
                     ],
+
+                    //Version of the profile
                     version: '1.0.0'
                 });
 
