afa8135811a576f8d0ab155f8317090d972d3f5a
Florian Lenz <flenz81@googlemail.com>
2017-11-04 13:39:31 +0100

    [errors] exported errors with export keyword and removed not needed errors

--- src/errors.js
+++ src/errors.js
@@ -1,49 +1,22 @@
 // @flow
 
-class InvalidPrivateKeyError extends Error{}
+export class InvalidPrivateKeyError extends Error{}
 
-class PasswordMismatch extends Error{}
+export class PasswordMismatch extends Error{}
 
-class PasswordContainsSpecialChars extends Error{}
+export class PasswordContainsSpecialChars extends Error{}
 
 // Is thrown when there is no private key for an address
-class NoEquivalentPrivateKey extends Error{}
+export class NoEquivalentPrivateKey extends Error{}
 
-class InvalidEncryptionAlgorithm extends Error{}
+export class InvalidEncryptionAlgorithm extends Error{}
 
-class FailedToDecryptPrivateKeyPasswordInvalid extends Error{}
+export class FailedToDecryptPrivateKeyPasswordInvalid extends Error{}
 
-class CanceledAction extends Error{}
+export class CanceledAction extends Error{}
 
-class DecryptedValueIsNotAPrivateKey extends Error{}
+export class DecryptedValueIsNotAPrivateKey extends Error{}
 
-class NoProfilePresent extends Error{}
+export class NoProfilePresent extends Error{}
 
-class NoPublicProfilePresent extends Error{}
-
-/**
- * Is thrown if a method on the secure storage implementation is missing
- */
-class UnsatisfiedSecureStorageImplementationError extends Error{
-
-    constructor(missingMethodName: string) {
-
-        super('Missing method: "'+missingMethodName+'" in secure storage implementation');
-
-    }
-
-}
-
-module.exports = {
-    InvalidPrivateKeyError,
-    UnsatisfiedSecureStorageImplementationError,
-    PasswordMismatch,
-    PasswordContainsSpecialChars,
-    NoEquivalentPrivateKey,
-    InvalidEncryptionAlgorithm,
-    FailedToDecryptPrivateKeyPasswordInvalid,
-    CanceledAction,
-    DecryptedValueIsNotAPrivateKey,
-    NoProfilePresent,
-    NoPublicProfilePresent
-};
+export class NoPublicProfilePresent extends Error{}
