36ae46eeb541c1004434ef9ce32065c3201b3c3e
Florian Lenz <flenz81@googlemail.com>
2017-11-04 12:14:57 +0100

    [tests] queries - added test for findProfiles

---
+++ src/database/queries.test.js
@@ -0,0 +1,32 @@
+const queries = require('./../../lib/database/queries');
+
+describe('queries', () => {
+    "use strict";
+
+    test('findProfiles', () => {
+
+        // Profiles mock data
+        const profiles = [
+            {
+                //Profile one
+            },
+            {
+                //Profile two
+            }
+        ];
+
+        const realmMock = {
+            objects: jest.fn()
+        };
+
+        realmMock
+            .objects
+            .mockReturnValueOnce(profiles);
+
+        expect(queries.findProfiles(realmMock)).toBe(profiles);
+        
+        expect(realmMock.objects).toBeCalledWith('Profile')
+
+    })
+
+});
