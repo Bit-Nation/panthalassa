79f27c74a7e98a306f5e90aed8479a44b4fa2082
Florian Lenz <flenz81@googlemail.com>
2018-01-14 20:11:17 +0100

    [eth] added test's for web3

--- src/ethereum/web3.js
+++ src/ethereum/web3.js
@@ -3,30 +3,32 @@
 import {JsonRpcNodeInterface} from '../specification/jsonRpcNode';
 import type {EthUtilsInterface} from './utils';
 import PanthalassaProvider from './PanthalassaProvider';
-const EventEmitter = require('eventemitter3');
 const Web3 = require('web3');
 
 /**
  * @name web3Factory
  * @param {JsonRpcNodeInterface} node
- * @param {EventEmitter} ee
  * @param {EthUtilsInterface} ethUtils
  * @param {boolean} networkAccess
  * @return {Promise} resolves with an web3 object when the node is started successfully. If the node fail to start the promise will be rejected.
  */
-export default function web3Factory(node: JsonRpcNodeInterface, ee: EventEmitter, ethUtils: EthUtilsInterface, networkAccess: boolean): Promise<Web3> {
+export default function web3Factory(node: JsonRpcNodeInterface, ethUtils: EthUtilsInterface, networkAccess: boolean): Promise<Web3> {
+
     return new Promise((res, rej) => {
         ethUtils
             .allKeyPairs()
             .then((keyPairsMap) => {
-                // the keys are valid ethereum addresses
-                const addresses = Object.keys(keyPairsMap);
 
-                if (networkAccess === true) {
+                if (networkAccess === false) {
                     const web3 = new Web3();
 
-                    if (addresses.length !== 0) {
-                        web3.eth.defaultAccount = addresses[0];
+                    //Even when creating an new instance of Web3
+                    //the default account stay's so we need to reset it.
+                    //In case it was previously set.
+                    web3.eth.defaultAccount = undefined;
+
+                    if (keyPairsMap.size !== 0) {
+                        web3.eth.defaultAccount = keyPairsMap.keys().next().value;
                     }
 
                     return res(web3);
@@ -35,13 +37,19 @@ export default function web3Factory(node: JsonRpcNodeInterface, ee: EventEmitter
                 // Start the ethereum node
                 node
                     .start()
-                    .then((_) => {
+                    .then(_ => {
+
                         const provider = new PanthalassaProvider(ethUtils, node.url);
 
                         const web3 = new Web3(provider);
 
-                        if (addresses.length !== 0) {
-                            web3.eth.defaultAccount = addresses[0];
+                        //Even when creating an new instance of Web3
+                        //the default account stay's so we need to reset it.
+                        //In case it was previously set.
+                        web3.eth.defaultAccount = undefined;
+
+                        if (keyPairsMap.size !== 0) {
+                            web3.eth.defaultAccount = keyPairsMap.keys().next().value;
                         }
 
                         return res(web3);
