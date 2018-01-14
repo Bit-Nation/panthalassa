d2dd01490ac2231aaeb48c1118631cbe43e40dec
Florian Lenz <flenz81@googlemail.com>
2018-01-14 18:11:24 +0100

    [eth] changed provider to return different instances based on if there is a network connection or not

--- src/ethereum/web3.js
+++ src/ethereum/web3.js
@@ -11,24 +11,50 @@ const Web3 = require('web3');
  * @param {JsonRpcNodeInterface} node
  * @param {EventEmitter} ee
  * @param {EthUtilsInterface} ethUtils
+ * @param {boolean} networkAccess
  * @return {Promise} resolves with an web3 object when the node is started successfully. If the node fail to start the promise will be rejected.
  */
-export default function web3Factory(node: JsonRpcNodeInterface, ee: EventEmitter, ethUtils: EthUtilsInterface): Promise<Web3> {
+export default function web3Factory(node: JsonRpcNodeInterface, ee: EventEmitter, ethUtils: EthUtilsInterface, networkAccess: boolean): Promise<Web3> {
     return new Promise((res, rej) => {
-        // Start the ethereum node
-        node
-            .start()
-            .then((_) => {
-                const provider = new PanthalassaProvider(ethUtils, node.url);
 
-                provider.on('error', (error) => ee.emit('eth:node:error', {error: error}));
+        ethUtils
+            .allKeyPairs()
+            .then((keyPairsMap) => {
+                // the keys are valid ethereum addresses
+                const addresses = Object.keys(keyPairsMap);
 
-                res(new Web3(provider));
+                if(networkAccess === true){
 
-                ee.emit('eth:node:start:success');
+                    const web3 = new Web3();
+
+                    if(addresses.length !== 0){
+                        web3.eth.defaultAccount = addresses[0]
+                    }
+
+                    return res(web3);
+
+                }
+
+                // Start the ethereum node
+                node
+                    .start()
+                    .then(_ => {
+
+                        const provider = new PanthalassaProvider(ethUtils, node.url);
+
+                        const web3 = new Web3(provider);
+
+                        if(addresses.length !== 0){
+                            web3.eth.defaultAccount = addresses[0];
+                        }
+
+                        return res(web3)
+
+                    })
+                    .catch(rej);
+
             })
-            .catch((error) => ee.emit('eth:node:start:failed', {
-                error: error,
-            }));
+            .catch(rej);
+
     });
 }
