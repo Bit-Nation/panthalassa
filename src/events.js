ef9c4669fd762a0689e3e2c983aca5439b401333
Florian Lenz <flenz81@googlemail.com>
2018-01-14 18:10:03 +0100

    [project] added events file, holding all event's

---
+++ src/events.js
@@ -0,0 +1,17 @@
+/**
+ * @desc should be dispatched when the client goes online
+ * @type {string}
+ */
+export const APP_ONLINE = 'app:online';
+
+/**
+ * @desc should be dispatched when the client goes offline
+ * @type {string}
+ */
+export const APP_OFFLINE = 'app:offline';
+
+/**
+ * @desc Should be dispatched when the amount of addresses changes
+ * @type {string}
+ */
+export const AMOUNT_OF_ADDRESSES_CHANGED = 'amount_of_addresses_changed';
