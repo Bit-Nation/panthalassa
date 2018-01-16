249eac36d8ccc86e452ee19eab3c35822fc69ff8
Florian Lenz <flenz81@googlemail.com>
2018-01-16 13:00:28 +0100

    [events] fixed spelling mistake

--- src/events.js
+++ src/events.js
@@ -15,3 +15,9 @@ export const APP_OFFLINE = 'app:offline';
  * @type {string}
  */
 export const AMOUNT_OF_ADDRESSES_CHANGED = 'amount_of_addresses_changed';
+
+/**
+ * @desc Should be dispatched when a job was added to the messaging queue
+ * @type {string}
+ */
+export const MESSAGING_QUEUE_JOB_ADDED = 'messaging_queue:job:added';
