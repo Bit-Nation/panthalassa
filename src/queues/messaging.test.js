3f800ec6c82ff7ffe96b3000215566a6b6259443
Florian Lenz <flenz81@googlemail.com>
2018-01-16 13:13:26 +0100

    [queues] messagingQueue test's added addJob write to database

--- src/queues/messaging.test.js
+++ src/queues/messaging.test.js
@@ -31,7 +31,39 @@ describe('messaging', () => {
 
         });
 
-        test('save to database', () => {
+        test('save to database', (done) => {
+
+            const db = dbFactory(createDbPath());
+
+            const queue = messagingQueueFactory(new EventEmitter(), db);
+
+            queue
+                .addJob('Nation', 'Your nation ABC was created successfully')
+                .then(_ => {
+
+                    //Query the database for the job
+
+                    db
+                        .query(function (realm) {
+                            return realm.objects('MessageJob')
+                        })
+                        .then(result => {
+
+                            expect(result.length).toBe(1);
+
+                            expect(result[0].id).toBe(1);
+                            expect(result[0].heading).toBe('Nation');
+                            expect(result[0].text).toBe('Your nation ABC was created successfully');
+                            expect(result[0].version).toBe(1);
+
+                            done();
+
+                        })
+                        .catch(done.fail)
+
+                })
+                .catch(done.fail)
+
 
         })
 
