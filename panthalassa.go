de1dafa32ef1afc55176f193070d736d4519cbd9
Florian Lenz <flenz81@googlemail.com>
2018-04-04 15:55:25 +0200

    [panthalassa] added new root key method

--- panthalassa.go
+++ panthalassa.go
@@ -1,1 +1,23 @@
 package panthalassa
+
+import (
+	rootKey "github.com/Bit-Nation/panthalassa/rootKey"
+)
+
+//Create's a new root key
+func NewRootKey(pw string) (string, error) {
+
+	rk, err := rootKey.NewRootKey()
+	if err != nil {
+		return "", err
+	}
+
+	exportedRootKey, err := rk.Export(pw)
+
+	if err != nil {
+		return "", err
+	}
+
+	return exportedRootKey, nil
+
+}
