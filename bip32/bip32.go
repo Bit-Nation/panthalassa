1294db4f6d75e7e50189f712a6a4513ed4b37026
Florian Lenz <flenz81@googlemail.com>
2018-04-04 18:40:06 +0200

    [bip32] implemented NewMasterKey

--- bip32/bip32.go
+++ bip32/bip32.go
@@ -1,9 +1,11 @@
 package bip32
 
 import (
-	bip32 "github.com/tyler-smith/go-bip32"
+	b32 "github.com/tyler-smith/go-bip32"
 )
 
-func adsf() {
-	bip32.NewMasterKey()
+type Key = b32.Key
+
+func NewMasterKey(seed []byte) (*b32.Key, error) {
+	return b32.NewMasterKey(seed)
 }
