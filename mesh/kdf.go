1a79aa45f51e555089a07d041ea27886ae060150
Florian Lenz <flenz81@googlemail.com>
2018-04-04 10:58:41 +0200

    [kdf] fixed import's

--- mesh/kdf.go
+++ mesh/kdf.go
@@ -1,14 +1,14 @@
 package mesh
 
 import (
-	"gx/ipfs/QmZyZDi491cCNTLfAhwcaDii2Kg4pwKRkhqQzURGDvY6ua/go-multihash"
-	"gx/ipfs/QmcZfnkapfECQGcLZaf9B79NRg7cRa9EnZh4LSbkCzwNvY/go-cid"
+	mh "github.com/multiformats/go-multihash"
+	cid "github.com/ipfs/go-cid"
 )
 
 //Generate the rendezvous content id
 //@todo chose something that involves time or a version on pangea
 func rendezvousKey(seed string) (*cid.Cid, error) {
 
-	return cid.NewPrefixV1(cid.Raw, multihash.SHA3_256).Sum([]byte(seed))
+	return cid.NewPrefixV1(cid.Raw, mh.SHA3_256).Sum([]byte(seed))
 
 }
