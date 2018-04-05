62809984d81d96bd3bac81c08614b321ce8743b1
Florian Lenz <flenz81@googlemail.com>
2018-04-05 13:58:48 +0200

    [bip32] updated bip32.go file and added description

--- bip32/bip32.go
+++ bip32/bip32.go
@@ -1,11 +1,13 @@
 package bip32
 
 import (
-	b32 "github.com/tyler-smith/go-bip32"
+	bip32 "github.com/tyler-smith/go-bip32"
 )
 
-type Key = b32.Key
+type Key = bip32.Key
 
-func NewMasterKey(seed []byte) (*b32.Key, error) {
-	return b32.NewMasterKey(seed)
+//Create new MasterKey as specified in bip32
+//where child key's can be derived
+func NewMasterKey(seed []byte) (*bip32.Key, error) {
+	return bip32.NewMasterKey(seed)
 }
