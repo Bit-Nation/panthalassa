f2a999887b93ed7190c22c89a1f5e0892abe8caf
Florian Lenz <flenz81@googlemail.com>
2018-04-05 18:37:34 +0200

    [key_manager] removed ethereum key for now

--- keyManager/keyManager.go
+++ keyManager/keyManager.go
@@ -1,19 +1,11 @@
 package keyManager
 
 import (
-	"encoding/hex"
-	bip32 "github.com/Bit-Nation/panthalassa/bip32"
 	rk "github.com/Bit-Nation/panthalassa/rootKey"
 )
 
 type KeyManager struct {
 	rootKey rk.RootKey
-	ethKey  bip32.Key
-}
-
-//Encode the eth bip32 key to a hex string
-func (kM KeyManager) GetEthPrivateKey() string {
-	return hex.EncodeToString(kM.ethKey.Key)
 }
 
 //Create a new key manager from the encrypted root key
@@ -25,25 +17,8 @@ func NewKeyManager(encryptedRootKey, pw string) (*KeyManager, error) {
 		return &KeyManager{}, nil
 	}
 
-	masterKey, err := bip32.NewMasterKey([]byte(rootKey.GetWithWhiteSpace()))
-
-	if err != nil {
-		return &KeyManager{}, err
-	}
-
-	//Create ethereum key as specified
-	ethKey, err := masterKey.NewChildKey(2 ^ 31 + 100)
-	if err != nil {
-		return &KeyManager{}, err
-	}
-	ethKey, err = ethKey.NewChildKey(2 ^ 31 + 10)
-	if err != nil {
-		return &KeyManager{}, err
-	}
-
 	keyManager := KeyManager{
 		rootKey: rootKey,
-		ethKey:  *ethKey,
 	}
 
 	return &keyManager, nil
