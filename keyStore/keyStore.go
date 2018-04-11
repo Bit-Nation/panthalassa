1871581392f75e35ad21fc5327f89ea601e779c6
Florian Lenz <flenz81@googlemail.com>
2018-04-11 16:15:07 +0200

    [tests] added mooooaaar test's to keyStore

--- keyStore/keyStore.go
+++ keyStore/keyStore.go
@@ -1,6 +1,7 @@
 package keyStore
 
 import (
+	"encoding/hex"
 	jsonUtil "encoding/json"
 	"errors"
 	bip32 "github.com/Bit-Nation/panthalassa/bip32"
@@ -10,15 +11,26 @@ import (
 //Ethereum private key validation rule
 var ethPrivateKeyValidation = func(store KeyStore) error {
 
-	//derive ethereum key
-	seed := bip39.NewSeed(store.mnemonic, "pangea")
+	//derive seed used for coins
+	seed := bip39.NewSeed(store.mnemonic, "coins")
+	//make it the master key
 	key, err := bip32.NewMasterKey(seed)
 	if err != nil {
 		return err
 	}
+	//Derive the ethereum key as per spec
 	key, err = bip32.Derive("m/100H/10H", key)
 	if err != nil {
 		return err
+	}
+
+	//compare key's
+	hexKey, err := store.GetKey("eth_private_key")
+	if err != nil {
+		return err
+	}
+	if hex.EncodeToString(key.Key) != hexKey {
+		return errors.New("derivation mismatch - ethereum private key from storage and derived one doesn't match")
 	}
 
 	return nil
@@ -32,9 +44,16 @@ var validationRules = map[uint8][]func(ks KeyStore) error{
 }
 
 type KeyStore struct {
-	mnemonic string            `json:mnemonic`
-	keys     map[string]string `json:keys`
-	version  uint8             `json:version`
+	mnemonic string            `json:"mnemonic"`
+	keys     map[string]string `json:"keys"`
+	version  uint8             `json:"version"`
+}
+
+//Only used for json marshalling
+type jsonKeyStore struct {
+	Mnemonic string            `json:"mnemonic"`
+	Keys     map[string]string `json:"keys"`
+	Version  uint8             `json:"version"`
 }
 
 func (ks KeyStore) GetKey(key string) (string, error) {
@@ -57,9 +76,9 @@ func (ks KeyStore) validate() error {
 	}
 
 	//Validate the key store
-	for _, f := range validationRules[ks.version] {
-		if f(ks) != nil {
-			return f(ks)
+	for _, vR := range validationRules[ks.version] {
+		if err := vR(ks); err != nil {
+			return err
 		}
 	}
 
@@ -68,22 +87,41 @@ func (ks KeyStore) validate() error {
 
 //Marshal key store
 func (ks KeyStore) Marshal() ([]byte, error) {
-	return jsonUtil.Marshal(ks)
+
+	if err := ks.validate(); err != nil {
+		return []byte{}, err
+	}
+
+	jsk := jsonKeyStore{
+		Mnemonic: ks.mnemonic,
+		Keys:     ks.keys,
+		Version:  ks.version,
+	}
+
+	return jsonUtil.Marshal(jsk)
 }
 
 //Convert json keystore to object
 func FromJson(json string) (*KeyStore, error) {
-	var keyStore KeyStore
-	err := jsonUtil.Unmarshal([]byte(json), &keyStore)
+	var jsk jsonKeyStore
+	err := jsonUtil.Unmarshal([]byte(json), &jsk)
 	if err != nil {
-		return &keyStore, err
-	}
-
-	if err := keyStore.validate(); err != nil {
 		return &KeyStore{}, err
 	}
 
-	return &KeyStore{}, nil
+	//Create keystore form parsed json
+	ks := KeyStore{
+		mnemonic: jsk.Mnemonic,
+		keys:     jsk.Keys,
+		version:  jsk.Version,
+	}
+
+	//Exit on invalid key store
+	if err := ks.validate(); err != nil {
+		return &KeyStore{}, err
+	}
+
+	return &ks, nil
 }
 
 func NewMnemonic(mnemonic string) {
