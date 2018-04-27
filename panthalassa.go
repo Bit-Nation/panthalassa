935370c98c1b5ba2c3f04284806fc16de21b9381
Florian Lenz <flenz81@googlemail.com>
2018-04-27 09:29:48 +0200

    [mobile] added utils to create panthalassa instance on mobile

--- panthalassa.go
+++ panthalassa.go
@@ -4,36 +4,36 @@ import (
 	"github.com/Bit-Nation/panthalassa/keyManager"
 )
 
-type Panthalassa struct {
+type panthalassa struct {
 	km *keyManager.KeyManager
 }
 
 //Get ethereum private key of panthalassa instance
-func (p Panthalassa) EthereumPrivateKey() (string, error) {
+func (p panthalassa) EthereumPrivateKey() (string, error) {
 	return p.km.GetEthereumPrivateKey()
 }
 
 //Stop the panthalassa instance
 //this becomes interesting when we start
 //to use the mesh network
-func (p *Panthalassa) Stop() error {
+func (p *panthalassa) Stop() error {
 	return nil
 }
 
 //Export account with the given password
-func (p *Panthalassa) Export(pw, pwConfirm string) (string, error) {
+func (p *panthalassa) Export(pw, pwConfirm string) (string, error) {
 	return p.km.Export(pw, pwConfirm)
 }
 
 //Create an new instance of panthalassa
-func NewPanthalassa(encryptedAccount, pw string) (*Panthalassa, error) {
+func newPanthalassa(encryptedAccount, pw string) (*panthalassa, error) {
 
 	km, err := keyManager.OpenWithPassword(encryptedAccount, pw)
 	if err != nil {
-		return &Panthalassa{}, nil
+		return &panthalassa{}, nil
 	}
 
-	return &Panthalassa{
+	return &panthalassa{
 		km: km,
 	}, nil
 }
