3fd0fe2f9092bc5d64f9bd7bdd2ad1fc64271cef
Florian Lenz <flenz81@googlemail.com>
2018-05-03 09:24:54 +0200

    [bip39] added method to validate mnemonic

--- bip39/bip39.go
+++ bip39/bip39.go
@@ -3,6 +3,7 @@ package bip39
 import (
 	"crypto/rand"
 	bip39 "github.com/tyler-smith/go-bip39"
+	"strings"
 )
 
 //Wordlist from bip30 package
@@ -27,4 +28,36 @@ func NewMnemonic() (string, error) {
 //Generate new seed of mnemonic and password
 func NewSeed(mnemonic string, password string) []byte {
 	return bip39.NewSeed(mnemonic, password)
+}
+
+//Check if an mnemonic is valid or not
+func ValidMnemonic(mnemonic string) bool {
+
+	words := strings.Split(mnemonic, " ")
+
+	if len(words) != 24 {
+		return false
+	}
+
+	for _, word := range words {
+
+		exist := false
+
+		for _, bip39Word := range EnglishWordList {
+
+			if word == bip39Word {
+				exist = true
+				break
+			}
+
+		}
+
+		if !exist {
+			return false
+		}
+
+	}
+
+	return true
+
 }
