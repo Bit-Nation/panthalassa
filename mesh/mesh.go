747a1acec2b24215e962f42bd37beb434d66b45d
Florian Lenz <flenz81@googlemail.com>
2018-05-13 15:15:19 +0200

    [mesh] removed protocols

--- mesh/mesh.go
+++ mesh/mesh.go
@@ -3,10 +3,8 @@ package mesh
 import (
 	"context"
 
-	pangeaProtocol "github.com/Bit-Nation/panthalassa_new/protocols/pangea"
 	libp2p "github.com/libp2p/go-libp2p"
 	host "github.com/libp2p/go-libp2p-host"
-	net "github.com/libp2p/go-libp2p-net"
 )
 
 //configuration for mesh network
@@ -25,9 +23,6 @@ func NewMesh() (*Mesh, error) {
 
 	m := Mesh{
 		close: make(chan struct{}, 1),
-		protocols: []Protocol{
-			&pangeaProtocol.Protocol{},
-		},
 	}
 
 	//Create host
@@ -36,18 +31,6 @@ func NewMesh() (*Mesh, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	//Register protocols
-	for _, p := range m.protocols {
-		p.Register(h)
-	}
-
-	//Register network listener
-	h.Network().Notify(&NotifyBundle{
-		OpenedStreamF: func(_ net.Network, stream net.Stream) {
-
-		},
-	})
 
 	return &m, nil
 }
