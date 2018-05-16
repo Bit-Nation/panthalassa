9e0d06487ac1037da7a6b320ff9f1863263ede07
Florian Lenz <flenz81@googlemail.com>
2018-05-16 13:24:48 +0200

    [rendezvous] added type to derivce rendezvous key's based on a seed

---
+++ mesh/rendezvous.go
@@ -0,0 +1,21 @@
+package mesh
+
+import (
+	cid "github.com/ipfs/go-cid"
+	mh "github.com/multiformats/go-multihash"
+)
+
+type RendezvousKey struct {
+	key []byte
+}
+
+func (r *RendezvousKey) Derive(key string) (*cid.Cid, error) {
+	final := append(r.key, []byte(key)...)
+	return cid.NewPrefixV1(cid.Raw, mh.SHA3_256).Sum(final)
+}
+
+func NewRendezvousKey(seed string) RendezvousKey {
+	return RendezvousKey{
+		key: []byte(seed),
+	}
+}
