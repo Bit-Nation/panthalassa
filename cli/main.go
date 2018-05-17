74819014a048f20f0f0802ed153825a9140e20fa
Florian Lenz <flenz81@googlemail.com>
2018-05-17 20:10:27 +0200

    [cli] added creation of account

--- cli/main.go
+++ cli/main.go
@@ -8,26 +8,37 @@ import (
 	mnemonic "github.com/Bit-Nation/panthalassa/mnemonic"
 	panthalassa "github.com/Bit-Nation/panthalassa/mobile"
 	log "github.com/ipfs/go-log"
-	ishell "gopkg.in/abiosoft/ishell.v2"
+	jsonDB "github.com/nanobox-io/golang-scribble"
+	uuid "github.com/satori/go.uuid"
+	iShell "gopkg.in/abiosoft/ishell.v2"
 )
 
 const DevRendezvousKey = "akhgp58izorhalsdipfo3uh5orpawoudshfalskduf43topa"
 const LogFile = "log.out"
+const DBName = ".database"
 
 var logger = log.Logger("hi")
 
+type Account struct {
+	Name         string `json:"name"`
+	AccountStore string `json:"account_store"`
+}
+
 func main() {
 
-	shell := ishell.New()
+	//Database
+	db, err := jsonDB.New(DBName, nil)
+	if err != nil {
+		panic(err)
+	}
 
-	// display welcome info.
-	shell.Println("Panthalassa interactive shell")
+	shell := iShell.New()
 
 	// register command to start panthalassa
-	shell.AddCmd(&ishell.Cmd{
+	shell.AddCmd(&iShell.Cmd{
 		Name: "start",
 		Help: "start panthalassa",
-		Func: func(c *ishell.Context) {
+		Func: func(c *iShell.Context) {
 
 			mne, err := mnemonic.New()
 			if err != nil {
@@ -56,10 +67,10 @@ func main() {
 	})
 
 	// stop panthalassa
-	shell.AddCmd(&ishell.Cmd{
+	shell.AddCmd(&iShell.Cmd{
 		Name: "stop",
 		Help: "stop's the current panthalassa instance",
-		Func: func(c *ishell.Context) {
+		Func: func(c *iShell.Context) {
 			err := panthalassa.Stop()
 			if err != nil {
 				c.Err(err)
@@ -70,10 +81,10 @@ func main() {
 	})
 
 	// display private key
-	shell.AddCmd(&ishell.Cmd{
+	shell.AddCmd(&iShell.Cmd{
 		Name: "eth:private",
 		Help: "show's the ethereum private key",
-		Func: func(c *ishell.Context) {
+		Func: func(c *iShell.Context) {
 			pk, err := panthalassa.EthPrivateKey()
 			if err != nil {
 				c.Err(err)
@@ -84,10 +95,10 @@ func main() {
 	})
 
 	// display address
-	shell.AddCmd(&ishell.Cmd{
+	shell.AddCmd(&iShell.Cmd{
 		Name: "eth:address",
 		Help: "display ethereum address",
-		Func: func(c *ishell.Context) {
+		Func: func(c *iShell.Context) {
 			addr, err := panthalassa.EthAddress()
 			if err != nil {
 				c.Err(err)
@@ -99,10 +110,10 @@ func main() {
 		},
 	})
 
-	shell.AddCmd(&ishell.Cmd{
+	shell.AddCmd(&iShell.Cmd{
 		Name: "log:debug",
 		Help: "Enable debug logging",
-		Func: func(c *ishell.Context) {
+		Func: func(c *iShell.Context) {
 			f, err := os.Create(LogFile)
 			if err != nil {
 				c.Err(err)
@@ -113,10 +124,10 @@ func main() {
 		},
 	})
 
-	shell.AddCmd(&ishell.Cmd{
+	shell.AddCmd(&iShell.Cmd{
 		Name: "log:warn",
 		Help: "Enable debug logging",
-		Func: func(c *ishell.Context) {
+		Func: func(c *iShell.Context) {
 			f, err := os.Create(LogFile)
 			if err != nil {
 				c.Err(err)
@@ -126,6 +137,63 @@ func main() {
 			//2 = WARNING
 			log.SetAllLoggers(2)
 			c.Println("Enabled logging (for warning's). Output file: ", f.Name())
+		},
+	})
+
+	shell.AddCmd(&iShell.Cmd{
+		Name: "account:new",
+		Help: "Create a new Account",
+		Func: func(c *iShell.Context) {
+
+			//get username
+			c.Println("Account name: ")
+			accountName := c.ReadLine()
+
+			// get password
+			c.Print("Password for account: ")
+			password := c.ReadLine()
+
+			// create mnemonic
+			mne, err := mnemonic.New()
+			if err != nil {
+				c.Err(err)
+				return
+			}
+
+			// create key store form mnemonic
+			store, err := ks.NewFromMnemonic(mne)
+			if err != nil {
+				c.Err(err)
+				return
+			}
+
+			// create key manager from key store
+			keyManager := km.CreateFromKeyStore(store)
+			exportedAccount, err := keyManager.Export(password, password)
+			if err != nil {
+				c.Err(err)
+				return
+			}
+
+			// uuid
+			id, err := uuid.NewV4()
+			if err != nil {
+				c.Err(err)
+				return
+			}
+
+			err = db.Write("account", id.String(), &Account{
+				Name:         accountName,
+				AccountStore: exportedAccount,
+			})
+
+			if err != nil {
+				c.Err(err)
+				return
+			}
+
+			c.Println("safed account store")
+
 		},
 	})
 
