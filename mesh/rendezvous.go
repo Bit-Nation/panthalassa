5eb899e3f318d132bbbb50d381636c780fb43573
Florian Lenz <flenz81@googlemail.com>
2018-05-21 21:53:40 +0200

    [mesh] added rendezvous key for profile

--- mesh/rendezvous.go
+++ mesh/rendezvous.go
@@ -2,6 +2,7 @@ package mesh
 
 import (
 	cid "github.com/ipfs/go-cid"
+	lp2pCrypto "github.com/libp2p/go-libp2p-crypto"
 	mh "github.com/multiformats/go-multihash"
 )
 
@@ -10,12 +11,25 @@ const (
 )
 
 type RendezvousKey struct {
+	// RK
 	key []byte
 }
 
 func (r *RendezvousKey) Derive(key string) (*cid.Cid, error) {
 	final := append(r.key, []byte(key)...)
 	return cid.NewPrefixV1(cid.Raw, mh.SHA3_256).Sum(final)
+}
+
+// generate profile key dht key for given public key
+// RK || "profile" || pubKey
+func (r RendezvousKey) Profile(pubKey lp2pCrypto.PubKey) (*cid.Cid, error) {
+	rawPubKey, err := pubKey.Bytes()
+	if err != nil {
+		return nil, err
+	}
+
+	key := append(r.key, []byte("profile")...)
+	return cid.NewPrefixV1(cid.Raw, mh.SHA3_256).Sum(append(key, rawPubKey...))
 }
 
 func NewRendezvousKey(seed string) RendezvousKey {
