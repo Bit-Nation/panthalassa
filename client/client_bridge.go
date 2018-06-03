f2f6dcbdd52df93c6f4a3df8615d1dafc751be98
Florian Lenz <flenz81@googlemail.com>
2018-06-03 16:33:16 +0300

    [client] fixed small bugs

--- client/client_bridge.go
+++ client/client_bridge.go
@@ -4,20 +4,16 @@ import (
 	x3dh "github.com/Bit-Nation/x3dh"
 )
 
-type ClientBridge interface {
+type Bridge interface {
 	// will fetch the signed pre key
 	// the signed pre key will be encrypted. So make sure to decrypt it.
 	FetchSignedPreKey() (string, error)
 }
 
 type Client struct {
-	bridge ClientBridge
+	bridge Bridge
 }
 
 func (c *Client) FetchSignedPreKey() x3dh.KeyPair {
 	return x3dh.KeyPair{}
-}
-
-func (c *Client) InitializedChat() error {
-
 }
