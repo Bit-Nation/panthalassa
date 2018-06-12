92f9d59f4b67553393abb841763a935f82a6fc7b
Florian Lenz <flenz81@googlemail.com>
2018-06-12 11:19:56 +0300

    [api] added new api

---
+++ api/api_test.go
@@ -0,0 +1,81 @@
+package api
+
+import (
+	"testing"
+	"time"
+	
+	pb "github.com/Bit-Nation/panthalassa/api/pb"
+	require "github.com/stretchr/testify/require"
+	proto "github.com/golang/protobuf/proto"
+	
+)
+
+type testUpStream struct {
+	sendFn func(data string)
+}
+
+func (u *testUpStream) Send(data string) {
+	u.sendFn(data)
+}
+
+// make sure that requests are added / removed correct
+func TestAPI_addAndCutRequestWorks(t *testing.T) {
+
+	req := pb.Request{}
+	req.RequestID = "hi"
+	
+	// api
+	api := New(&testUpStream{})
+	
+	// make sure request doesn't exist
+	_, exist := api.requests["hi"]
+	require.False(t, exist)
+	
+	api.addRequest(&req)
+	
+	// make sure request does exist
+	_, exist = api.requests["hi"]
+	require.True(t, exist)
+	
+	// now cut request our of the stack and make sure it was removed
+	api.cutRequest("hi")
+	_, exist = api.requests["hi"]
+	require.False(t, exist)
+	
+}
+
+func TestRequestResponse(t *testing.T)  {
+	
+	dataChan := make(chan string)
+	
+	var receivedRequestID string
+	
+	// api
+	api := New(&testUpStream{
+		sendFn: func(data string) {
+			dataChan <- data
+		},
+	})
+	
+	go func() {
+		select {
+			case data := <-dataChan:
+				req := &pb.Request{}
+				if err := proto.Unmarshal([]byte(data), req); err != nil {
+					panic(err)
+				}
+				receivedRequestID = req.RequestID
+				out := api.Respond(req.RequestID, &pb.Response{
+					RequestID: req.RequestID,
+				}, nil, time.Second)
+				if out != nil {
+					panic("expected nil but got: " + out.Error())
+				}
+		}
+	}()
+	
+	resp, err := api.request(&pb.Request{}, time.Second)
+	require.Nil(t, err)
+	require.Equal(t, resp.Msg.RequestID, receivedRequestID)
+	
+}
---
+++ api/doc.go
@@ -0,0 +1,7 @@
+package api
+
+// This package provider a kind of api to the client.
+// In our case the client is our pangea app
+
+// The API struct provides a set of utils to make requests to the api
+// If you plan to create a new
