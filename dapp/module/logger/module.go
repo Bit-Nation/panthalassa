4684389f91834724afeb56442e549522d0a28876
Florian Lenz <flenz81@googlemail.com>
2018-06-17 15:43:37 +0300

    [dapp] changed log module to write to stream

--- dapp/module/logger/module.go
+++ dapp/module/logger/module.go
@@ -1,20 +1,62 @@
 package logger
 
 import (
+	"bufio"
 	"strings"
 
+	pb "github.com/Bit-Nation/panthalassa/dapp/registry/pb"
+	net "github.com/libp2p/go-libp2p-net"
+	mc "github.com/multiformats/go-multicodec"
+	protoMc "github.com/multiformats/go-multicodec/protobuf"
 	logger "github.com/op/go-logging"
 	otto "github.com/robertkrimen/otto"
 )
+
+type streamLogger struct {
+	writer  *bufio.Writer
+	encoder mc.Encoder
+}
+
+func (l *streamLogger) Write(data []byte) (int, error) {
+
+	msg := pb.Message{
+		Type: pb.Message_LOG,
+		Log:  data,
+	}
+
+	if err := l.encoder.Encode(msg); err != nil {
+		return 0, err
+	}
+
+	if err := l.writer.Flush(); err != nil {
+		return 0, err
+	}
+
+	return len(data), nil
+}
 
 type Logger struct {
 	logger *logger.Logger
 }
 
-func New(l *logger.Logger) *Logger {
+func New(l *logger.Logger, stream net.Stream) (*Logger, error) {
+
+	l, err := logger.GetLogger("")
+	if err != nil {
+		return nil, err
+	}
+
+	w := bufio.NewWriter(stream)
+	loo := &streamLogger{
+		writer:  bufio.NewWriter(stream),
+		encoder: protoMc.Multicodec(nil).Encoder(w),
+	}
+
+	l.SetBackend(logger.AddModuleLevel(logger.NewLogBackend(loo, "", 0)))
+
 	return &Logger{
 		logger: l,
-	}
+	}, nil
 }
 
 func (l *Logger) Name() string {
