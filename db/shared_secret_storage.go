eadb397a98278787df733a58f56fd433a1b9003f
Florian Lenz <flenz81@googlemail.com>
2018-07-11 18:20:26 +0300

    [db] added shared secret storage

---
+++ db/shared_secret_storage.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"time"
+
+	x3dh "github.com/Bit-Nation/x3dh"
+	ed25519 "golang.org/x/crypto/ed25519"
+)
+
+type SharedSecret struct {
+	X3dhSS            x3dh.SharedSecret
+	Accepted          bool
+	CreatedAt         time.Time
+	DestroyAt         time.Time
+	EphemeralKey      x3dh.PublicKey
+	UsedSignedPreKey  x3dh.PublicKey
+	UsedOneTimePreKey *x3dh.PublicKey
+}
+
+type SharedSecretStorage interface {
+	HasAny(key ed25519.PublicKey) (bool, error)
+	GetYoungest(key ed25519.PublicKey) (SharedSecret, error)
+	Put(key ed25519.PublicKey, proto x3dh.InitializedProtocol) error
+}
