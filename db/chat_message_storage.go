b7eb15310ac682eb84d9029fa94b7e7d850c0255
Florian Lenz <flenz81@googlemail.com>
2018-07-11 18:21:38 +0300

    [db] updated chat message storage

---
+++ db/chat_message_storage.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	bpb "github.com/Bit-Nation/protobuffers"
+	ed25519 "golang.org/x/crypto/ed25519"
+)
+
+var (
+	messagesBucketName       = []byte("chat")
+	messageStorageBucketName = []byte("messages")
+	orderBucketName          = []byte("order")
+)
+
+// message status
+type Status uint
+
+const (
+	StatusSent           Status = 100
+	StatusFailedToSend          = 200
+	StatusDelivered      Status = 300
+	StatusFailedToHandle Status = 400
+)
+
+type ChatMessageStorage interface {
+	PersistMessage(to ed25519.PublicKey, msg bpb.PlainChatMessage) error
+	UpdateStatus(partner ed25519.PublicKey, msgID string, newStatus Status) error
+}
