ac05cbf2f0e44230244378e16e188779bb08c065
Florian Lenz <flenz81@googlemail.com>
2018-07-13 10:19:19 +0300

    [chat] added user storage to chat

--- chat/chat.go
+++ chat/chat.go
@@ -1,6 +1,9 @@
 package chat
 
 import (
+	"time"
+
+	prekey "github.com/Bit-Nation/panthalassa/chat/prekey"
 	db "github.com/Bit-Nation/panthalassa/db"
 	keyManager "github.com/Bit-Nation/panthalassa/keyManager"
 	bpb "github.com/Bit-Nation/protobuffers"
@@ -9,21 +12,27 @@ import (
 	ed25519 "golang.org/x/crypto/ed25519"
 )
 
+const (
+	SignedPreKeyValidTimeFrame = time.Second * 3600 * 24 * 60
+)
+
 // represents the source were we get things
 // like the pre key bundles from
 type Backend interface {
 	FetchPreKeyBundle(userIDPubKey ed25519.PublicKey) (x3dh.PreKeyBundle, error)
 	SubmitMessage(msg bpb.ChatMessage) error
+	FetchSignedPreKey(userIdPubKey ed25519.PublicKey) (prekey.PreKey, error)
 }
 
 type Chat struct {
 	messageDB           db.ChatMessageStorage
 	backend             Backend
 	sharedSecStorage    db.SharedSecretStorage
-	x3dh                x3dh.X3dh
+	x3dh                *x3dh.X3dh
 	km                  *keyManager.KeyManager
 	drKeyStorage        dr.KeysStorage
 	signedPreKeyStorage db.SignedPreKeyStorage
+	userStorage         db.UserStorage
 }
 
 func NewChat(msgDB db.ChatMessageStorage) *Chat {
