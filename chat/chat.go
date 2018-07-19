0b875308f0b7c2bedf338b9665a35c4671ad32d6
Florian Lenz <flenz81@googlemail.com>
2018-07-19 18:47:23 +0300

    [chat] created db interface for chat

--- chat/chat.go
+++ chat/chat.go
@@ -3,20 +3,28 @@ package chat
 import (
 	"time"
 
-	backend "github.com/Bit-Nation/panthalassa/backend"
+	preKey "github.com/Bit-Nation/panthalassa/chat/prekey"
 	db "github.com/Bit-Nation/panthalassa/db"
 	keyManager "github.com/Bit-Nation/panthalassa/keyManager"
+	bpb "github.com/Bit-Nation/protobuffers"
 	x3dh "github.com/Bit-Nation/x3dh"
 	dr "github.com/tiabc/doubleratchet"
+	ed25519 "golang.org/x/crypto/ed25519"
 )
 
 const (
 	SignedPreKeyValidTimeFrame = time.Hour * 24 * 60
 )
 
+type Backend interface {
+	FetchPreKeyBundle(userIDPubKey ed25519.PublicKey) (x3dh.PreKeyBundle, error)
+	SubmitMessage(msg bpb.ChatMessage) error
+	FetchSignedPreKey(userIdPubKey ed25519.PublicKey) (preKey.PreKey, error)
+}
+
 type Chat struct {
 	messageDB            db.ChatMessageStorage
-	backend              backend.Backend
+	backend              Backend
 	sharedSecStorage     db.SharedSecretStorage
 	x3dh                 *x3dh.X3dh
 	km                   *keyManager.KeyManager
