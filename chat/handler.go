800b09f8d96e44a6169278958e5a26131e7a8aeb
Florian Lenz <flenz81@googlemail.com>
2018-07-23 11:41:32 +0300

    [chat] added handler for one time pre key request

--- chat/handler.go
+++ chat/handler.go
@@ -1,9 +1,13 @@
 package chat
 
 import (
+	"crypto/rand"
+	"errors"
 	"sync"
 
+	preKey "github.com/Bit-Nation/panthalassa/chat/prekey"
 	bpb "github.com/Bit-Nation/protobuffers"
+	x3dh "github.com/Bit-Nation/x3dh"
 )
 
 // handles a set of protobuf messages
@@ -26,5 +30,63 @@ func (c *Chat) messagesHandler(req *bpb.BackendMessage_Request) (*bpb.BackendMes
 	}
 
 	return nil, nil
+
+}
+
+// handle new one time pre keys request
+func (c *Chat) oneTimePreKeys(req *bpb.BackendMessage_Request) (*bpb.BackendMessage_Response, error) {
+
+	// exit if not a request to fetch new one time pre keys
+	if req.NewOneTimePreKeys == 0 {
+		return nil, nil
+	}
+
+	// make sure request is valid
+	if req.NewOneTimePreKeys > 100 {
+		return nil, errors.New("requested more then the max allowed pre keys")
+	}
+
+	curve := x3dh.NewCurve25519(rand.Reader)
+
+	// generate key pairs
+	keyPairs := []x3dh.KeyPair{}
+	for {
+		if len(keyPairs) == int(req.NewOneTimePreKeys) {
+			break
+		}
+		keyPair, err := curve.GenerateKeyPair()
+		if err != nil {
+			return nil, err
+		}
+		keyPairs = append(keyPairs, keyPair)
+	}
+
+	// persist all key pairs
+	if err := c.oneTimePreKeyStorage.Put(keyPairs); err != nil {
+		logger.Error(err)
+		return nil, errors.New("failed to persist generated key pairs")
+	}
+
+	preKeys := []*bpb.PreKey{}
+
+	// convert and sign one time pre keys
+	for _, oneTimePreKey := range keyPairs {
+		pk := preKey.PreKey{}
+		pk.PublicKey = oneTimePreKey.PublicKey
+		if err := pk.Sign(*c.km); err != nil {
+			logger.Error(err)
+			return nil, errors.New("failed to sign one time pre key")
+		}
+		pkProto, err := pk.ToProtobuf()
+		if err != nil {
+			logger.Error(err)
+			return nil, errors.New("failed to convert pre key to protobuf")
+		}
+		preKeys = append(preKeys, &pkProto)
+	}
+
+	return &bpb.BackendMessage_Response{
+		OneTimePrekeys: preKeys,
+	}, nil
 
 }
