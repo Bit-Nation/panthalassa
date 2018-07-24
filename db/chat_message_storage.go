fd3927b471ed18df1ea38e1234ff56ef27c3305a
Florian Lenz <flenz81@googlemail.com>
2018-07-24 10:46:15 +0300

    [db] updated chat messages

--- db/chat_message_storage.go
+++ db/chat_message_storage.go
@@ -1,14 +1,20 @@
 package db
 
 import (
+	"encoding/binary"
+	"encoding/json"
+	"errors"
+
+	aes "github.com/Bit-Nation/panthalassa/crypto/aes"
+	km "github.com/Bit-Nation/panthalassa/keyManager"
 	bpb "github.com/Bit-Nation/protobuffers"
+	bolt "github.com/coreos/bbolt"
+	proto "github.com/gogo/protobuf/proto"
 	ed25519 "golang.org/x/crypto/ed25519"
 )
 
 var (
-	messagesBucketName       = []byte("chat")
-	messageStorageBucketName = []byte("messages")
-	orderBucketName          = []byte("order")
+	privateChatBucketName = []byte("private_chat")
 )
 
 // message status
@@ -19,10 +25,214 @@ const (
 	StatusFailedToSend   Status = 200
 	StatusDelivered      Status = 300
 	StatusFailedToHandle Status = 400
+	StatusPersisted      Status = 500
+	StatusReceived       Status = 600
 )
 
 type ChatMessageStorage interface {
 	PersistSentMessage(partner ed25519.PublicKey, msg bpb.PlainChatMessage) error
 	PersistReceivedMessage(partner ed25519.PublicKey, msg bpb.PlainChatMessage) error
 	UpdateStatus(partner ed25519.PublicKey, msgID string, newStatus Status) error
+}
+
+type Message struct {
+	ID string `json:"message_id"`
+	// encrypted message
+	Message aes.CipherText `json:"message"`
+	Version uint32         `json:"version"`
+	Status  Status         `json:"status"`
+	// is a received message
+	Received bool `json:"received"`
+}
+
+type PlainMessagePersistedEvent struct {
+	Msg     bpb.PlainChatMessage
+	Partner ed25519.PublicKey
+	DBMsg   Message
+}
+
+type BoltChatMessageStorage struct {
+	db                  *bolt.DB
+	postPersistListener []chan PlainMessagePersistedEvent
+	km                  *km.KeyManager
+}
+
+func NewChatMessageStorage(db *bolt.DB, listeners []chan PlainMessagePersistedEvent, km *km.KeyManager) *BoltChatMessageStorage {
+	return &BoltChatMessageStorage{
+		db:                  db,
+		postPersistListener: listeners,
+		km:                  km,
+	}
+}
+
+func (s *BoltChatMessageStorage) PersistMessage(partner ed25519.PublicKey, msg bpb.PlainChatMessage, received bool, status Status) error {
+	return s.db.Update(func(tx *bolt.Tx) error {
+
+		// private chat bucket
+		privChatBucket, err := tx.CreateBucketIfNotExists(privateChatBucketName)
+		if err != nil {
+			return err
+		}
+
+		// create partner chat bucket
+		partnerBucket, err := privChatBucket.CreateBucketIfNotExists(partner)
+		if err != nil {
+			return err
+		}
+
+		// turn created at into bytes
+		createdAt := make([]byte, 8)
+		binary.BigEndian.PutUint64(createdAt, uint64(msg.CreatedAt))
+
+		// make sure it is not taken and adjust the time indexed timestamp
+		tried := 0
+		for {
+			fetchedMsg := partnerBucket.Get(createdAt)
+			if fetchedMsg == nil || tried == 1000 {
+				break
+			}
+			tried++
+			binary.BigEndian.PutUint64(createdAt, uint64(msg.CreatedAt+1))
+		}
+
+		// marshal message
+		rawProtoMsg, err := proto.Marshal(&msg)
+		if err != nil {
+			return err
+		}
+
+		// encrypt raw proto message
+		encryptedMessage, err := s.km.AESEncrypt(rawProtoMsg)
+		if err != nil {
+			return err
+		}
+
+		// database message
+		dbMessage := Message{
+			ID:       msg.MessageID,
+			Message:  encryptedMessage,
+			Version:  1,
+			Status:   status,
+			Received: received,
+		}
+
+		// marshal DB message
+		rawDBMessage, err := json.Marshal(dbMessage)
+		if err != nil {
+			return err
+		}
+
+		// tell listeners that we persisted the message
+		tx.OnCommit(func() {
+			for _, listener := range s.postPersistListener {
+				listener <- PlainMessagePersistedEvent{
+					Msg:     msg,
+					Partner: partner,
+					DBMsg:   dbMessage,
+				}
+			}
+		})
+
+		return partnerBucket.Put(createdAt, rawDBMessage)
+
+	})
+}
+
+// fetch all chat partners
+func (s *BoltChatMessageStorage) AllChats() ([]ed25519.PublicKey, error) {
+	chats := []ed25519.PublicKey{}
+	err := s.db.View(func(tx *bolt.Tx) error {
+
+		// all private chats
+		privateChats := tx.Bucket(privateChatBucketName)
+		if privateChats == nil {
+			return nil
+		}
+
+		return privateChats.ForEach(func(k, _ []byte) error {
+			if len(k) == 32 {
+				chats = append(chats, k)
+			}
+			return nil
+		})
+	})
+	return chats, err
+}
+
+func (s *BoltChatMessageStorage) Messages(partner ed25519.PublicKey, start int64, amount uint) (map[int64]Message, error) {
+
+	messages := map[int64]Message{}
+
+	err := s.db.View(func(tx *bolt.Tx) error {
+
+		// private chats
+		privChatsBucket := tx.Bucket(privateChatBucketName)
+		if privChatsBucket == nil {
+			return nil
+		}
+
+		// partner chat bucket
+		partnerBucket := privChatsBucket.Bucket(partner)
+		if partnerBucket == nil {
+			return nil
+		}
+
+		cursor := partnerBucket.Cursor()
+		var msgID int64
+		var rawMsg []byte
+
+		// jump to position
+		if start == 0 {
+			key, value := cursor.Last()
+			msgID = int64(binary.BigEndian.Uint64(key))
+			rawMsg = value
+		} else {
+			startBytes := make([]byte, 8)
+			binary.BigEndian.PutUint64(startBytes, uint64(start))
+			key, value := cursor.Seek(startBytes)
+			msgID = int64(binary.BigEndian.Uint64(key))
+			rawMsg = value
+		}
+
+		// unmarshal message
+		msg := Message{}
+		if err := json.Unmarshal(rawMsg, &msg); err != nil {
+			return err
+		}
+		messages[msgID] = msg
+
+		currentAmount := amount - 1
+		for {
+			if currentAmount == 0 {
+				break
+			}
+			currentAmount--
+			key, rawMsg := cursor.Prev()
+			if key == nil {
+				break
+			}
+			msg := Message{}
+			if err := json.Unmarshal(rawMsg, &msg); err != nil {
+				return err
+			}
+			messages[int64(binary.BigEndian.Uint64(key))] = msg
+		}
+
+		return nil
+	})
+
+	return messages, err
+
+}
+
+func (s *BoltChatMessageStorage) PersistSentMessage(partner ed25519.PublicKey, msg bpb.PlainChatMessage) error {
+	return s.PersistMessage(partner, msg, false, StatusPersisted)
+}
+
+func (s *BoltChatMessageStorage) PersistReceivedMessage(partner ed25519.PublicKey, msg bpb.PlainChatMessage) error {
+	return s.PersistMessage(partner, msg, true, StatusPersisted)
+}
+
+func (s *BoltChatMessageStorage) UpdateStatus(partner ed25519.PublicKey, msgID string, newStatus Status) error {
+	return errors.New("currently not implemented")
 }
---
+++ db/chat_message_storage_test.go
@@ -0,0 +1,99 @@
+package db
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"encoding/hex"
+	"encoding/json"
+	"testing"
+
+	"fmt"
+	bpb "github.com/Bit-Nation/protobuffers"
+	bolt "github.com/coreos/bbolt"
+	"github.com/gogo/protobuf/proto"
+	require "github.com/stretchr/testify/require"
+	ed25519 "golang.org/x/crypto/ed25519"
+)
+
+func TestBoltChatMessageStorage_PersistMessage(t *testing.T) {
+
+	// setup
+	db := createDB()
+	km := createKeyManager()
+	partner, _, err := ed25519.GenerateKey(rand.Reader)
+	require.Nil(t, err)
+
+	// event listener
+	eventListener := make(chan PlainMessagePersistedEvent, 1)
+
+	// persist message
+	storage := NewChatMessageStorage(db, []chan PlainMessagePersistedEvent{eventListener}, km)
+	err = storage.PersistMessage(partner, bpb.PlainChatMessage{
+		Message:   []byte("hi there"),
+		CreatedAt: 33,
+		MessageID: "the_message_id",
+	}, true, 300)
+	require.Nil(t, err)
+
+	// assertions
+	allChatPartners, err := storage.AllChats()
+	require.Nil(t, err)
+	require.Equal(t, hex.EncodeToString(partner), hex.EncodeToString(allChatPartners[0]))
+
+	err = db.View(func(tx *bolt.Tx) error {
+		// private messages
+		privateMessage := tx.Bucket(privateChatBucketName)
+		require.NotNil(t, privateMessage)
+
+		// partner
+		partnerBucket := privateMessage.Bucket(partner)
+		require.NotNil(t, partnerBucket)
+
+		// fetch message
+		messageKey := make([]byte, 8)
+		binary.BigEndian.PutUint64(messageKey, 33)
+		fetchedMessage := partnerBucket.Get(messageKey)
+
+		// unmarshal message
+		message := Message{}
+		if err := json.Unmarshal(fetchedMessage, &message); err != nil {
+			return err
+		}
+
+		// decrypted message
+		plainMessage, err := km.AESDecrypt(message.Message)
+		require.Nil(t, err)
+
+		protoMsg := bpb.PlainChatMessage{}
+		if err := proto.Unmarshal(plainMessage, &protoMsg); err != nil {
+			return err
+		}
+
+		// plain message
+		plainMsg := bpb.PlainChatMessage{}
+		require.Nil(t, proto.Unmarshal(plainMessage, &plainMsg))
+
+		// test database message
+		require.Equal(t, uint32(1), message.Version)
+		require.Equal(t, Status(300), message.Status)
+		require.Equal(t, "the_message_id", message.ID)
+		require.Equal(t, "hi there", string(plainMsg.Message))
+		require.Equal(t, true, message.Received)
+
+		// test event listener
+		persistedMessage := <-eventListener
+		require.Equal(t, "hi there", string(persistedMessage.Msg.Message))
+		require.Equal(t, int64(33), persistedMessage.Msg.CreatedAt)
+		require.Equal(t, "the_message_id", persistedMessage.Msg.MessageID)
+		require.Equal(t, "the_message_id", persistedMessage.DBMsg.ID)
+		require.Equal(t, Status(300), persistedMessage.DBMsg.Status)
+		require.Equal(t, uint32(1), persistedMessage.DBMsg.Version)
+		require.Equal(t, true, persistedMessage.DBMsg.Received)
+
+		return nil
+	})
+	require.Nil(t, err)
+
+	fmt.Println(storage.Messages(partner, int64(33), 10))
+
+}
