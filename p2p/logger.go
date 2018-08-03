d8c5a10cb7e25e1a84528b6ff7b593755db5c984
Florian Lenz <flenz81@googlemail.com>
2018-08-03 15:11:39 +0300

    [p2p] fixed log formtting

--- p2p/logger.go
+++ p2p/logger.go
@@ -2,35 +2,12 @@ package p2p
 
 import (
 	"context"
-	"encoding/json"
-	"io"
-	"log"
 
 	ps "github.com/libp2p/go-libp2p-peerstore"
 	goLog "github.com/whyrusleeping/go-logging"
 )
 
 const logProtocol = "/pangea/logger/1.0.0"
-
-// to this from the ipfs logger since GX caused strange re writes in the import path
-type politeJSONFormatter struct{}
-
-// Format encodes a logging.Record in JSON and writes it to Writer.
-func (f *politeJSONFormatter) Format(calldepth int, r *goLog.Record, w io.Writer) error {
-	entry := make(map[string]interface{})
-	entry["id"] = r.Id
-	entry["level"] = r.Level
-	entry["time"] = r.Time
-	entry["module"] = r.Module
-	entry["message"] = r.Message()
-	err := json.NewEncoder(w).Encode(entry)
-	if err != nil {
-		return err
-	}
-
-	w.Write([]byte{'\n'})
-	return nil
-}
 
 func (n *Network) ConnectLogger(pInfo ps.PeerInfo) error {
 
@@ -47,10 +24,16 @@ func (n *Network) ConnectLogger(pInfo ps.PeerInfo) error {
 	}
 
 	// set formatter
-	goLog.SetFormatter(&politeJSONFormatter{})
+	formatter, err := goLog.NewStringFormatter("%{time} - %{shortfile} (%{level}): %{module} %{message}")
+	if err != nil {
+		return err
+	}
+	goLog.SetFormatter(formatter)
 
 	// set log backend
-	goLog.SetBackend(goLog.NewLogBackend(str, "", log.Ltime|log.Llongfile))
+	b := goLog.NewLogBackend(str, "", 0)
+	b.Color = true
+	goLog.SetBackend(b)
 
 	return nil
 
