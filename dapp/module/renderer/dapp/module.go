6046838abc4fa181744314211ccb06e0a118c414
Florian Lenz <flenz81@googlemail.com>
2018-08-09 15:14:27 +0300

    [dapp] fixed dapp renderer go routines

--- dapp/module/renderer/dapp/module.go
+++ dapp/module/renderer/dapp/module.go
@@ -18,6 +18,7 @@ type Module struct {
 	rmCBChan           chan *chan error
 	// returns a cb chan from the stack
 	nextCBChan chan chan *chan error
+	closer     chan struct{}
 }
 
 var sysLog = log.Logger("renderer - dapp")
@@ -109,27 +110,7 @@ func (m *Module) OpenDApp(payload string) error {
 }
 
 func (m *Module) Close() error {
-	close(m.setOpenHandlerChan)
-	close(m.getOpenHandlerChan)
-	// close all open callback
-	for {
-		// fetch next response channel
-		respChan := make(chan *chan error)
-		m.nextCBChan <- respChan
-		cbChan := <-respChan
-
-		// exit if there is no next channel
-		if cbChan == nil {
-			break
-		}
-
-		// send error in response
-		*cbChan <- errors.New("closed the application")
-		m.rmCBChan <- cbChan
-	}
-	close(m.addCBChan)
-	close(m.rmCBChan)
-	close(m.nextCBChan)
+	m.closer <- struct{}{}
 	return nil
 }
 
@@ -142,6 +123,7 @@ func New(l *logger.Logger) *Module {
 		addCBChan:          make(chan *chan error),
 		rmCBChan:           make(chan *chan error),
 		nextCBChan:         make(chan chan *chan error),
+		closer:             make(chan struct{}),
 	}
 
 	go func() {
@@ -151,43 +133,19 @@ func New(l *logger.Logger) *Module {
 
 		for {
 
-			// exit if channels got closed
-			if m.setOpenHandlerChan == nil || m.getOpenHandlerChan == nil || m.addCBChan == nil || m.rmCBChan == nil || m.nextCBChan == nil {
-				return
-			}
-
 			select {
-			case h := <-m.setOpenHandlerChan:
-				//@todo somehow there is a bug related to otto were it leads to
-				//@todo that setOpenHandler is called twice but wil nil for the handler
-				//@todo once we get rid of otto we can remove this check
-				if h == nil {
-					continue
+			case <-m.closer:
+				for cbChan, _ := range cbChans {
+					*cbChan <- errors.New("closed the application")
 				}
+			case h := <-m.setOpenHandlerChan:
 				openHandler = h
 			case respChan := <-m.getOpenHandlerChan:
-				//@todo somehow there is a bug related to otto were it leads to
-				//@todo that setOpenHandler is called twice but wil nil for the handler
-				//@todo once we get rid of otto we can remove this check
-				if respChan == nil {
-					continue
-				}
 				respChan <- openHandler
 			case cb := <-m.addCBChan:
 				cbChans[cb] = true
 			case cb := <-m.rmCBChan:
 				delete(cbChans, cb)
-			case respChan := <-m.nextCBChan:
-				if len(cbChans) == 0 {
-					respChan <- nil
-					continue
-				}
-				// my a bit ugly, but couldn't think of how to fetch the next callback
-				// in another way from a map
-				for cbChan, _ := range cbChans {
-					respChan <- cbChan
-					break
-				}
 			}
 
 		}
