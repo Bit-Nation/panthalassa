02d978933d26bd1db04039845ff83aff2dc99717
Andrii Selivanov <seland2008@gmail.com>
2018-08-14 13:47:28 +0300

    [chat] Reverted adding random ID to the received message.

--- db/chat_message_storage.go
+++ db/chat_message_storage.go
@@ -388,15 +388,10 @@ func (s *BoltChatMessageStorage) AddListener(fn func(e MessagePersistedEvent)) {
 }
 
 func (s *BoltChatMessageStorage) PersistMessageToSend(partner ed25519.PublicKey, msg Message) error {
-
-	// I don't know if that id should be random uuid, but
-	// it was required, so I added it there.
-
 	id, err := uuid.NewV4()
 	if err != nil {
 		return err
 	}
-
 	myIdKeyStr, err := s.km.IdentityPublicKey()
 	if err != nil {
 		return err
@@ -414,11 +409,6 @@ func (s *BoltChatMessageStorage) PersistMessageToSend(partner ed25519.PublicKey,
 }
 
 func (s *BoltChatMessageStorage) PersistReceivedMessage(partner ed25519.PublicKey, msg Message) error {
-	id, err := uuid.NewV4()
-	if err != nil {
-		return err
-	}
-	msg.ID = id.String()
 	msg.Status = StatusPersisted
 	msg.Received = true
 	return s.persistMessage(partner, msg)
