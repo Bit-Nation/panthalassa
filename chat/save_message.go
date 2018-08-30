48d5a24f90e20f3db57e51de2408bccf42e2fc0c
Florian Lenz <flenz81@googlemail.com>
2018-08-30 01:06:04 -0300

    [chat] fixed save message

--- chat/save_message.go
+++ chat/save_message.go
@@ -1,10 +1,12 @@
 package chat
 
 import (
+	"errors"
 	"time"
-
+	"encoding/hex"
+	
 	db "github.com/Bit-Nation/panthalassa/db"
-	"github.com/kataras/iris/core/errors"
+	uuid "github.com/satori/go.uuid"
 	ed25519 "golang.org/x/crypto/ed25519"
 )
 
@@ -14,9 +16,27 @@ var nowAsUnix = func() int64 {
 
 // persist private message
 func (c *Chat) SavePrivateMessage(to ed25519.PublicKey, rawMessage []byte) error {
+	id, err := uuid.NewV4()
+	if err != nil {
+		return err
+	}
+
+	senderStr, err := c.km.IdentityPublicKey()
+	if err != nil {
+		return err
+	}
+	sender, err := hex.DecodeString(senderStr)
+	if err != nil {
+		return err
+	}
+
 	msg := db.Message{
+		ID:        id.String(),
 		Message:   rawMessage,
 		CreatedAt: nowAsUnix(),
+		Version:   1,
+		Status:    db.StatusPersisted,
+		Sender:    sender,
 	}
 	// fetch chat
 	chat, err := c.chatStorage.GetChat(to)
