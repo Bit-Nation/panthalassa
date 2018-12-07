4ade4cfbaf832d55af993219790f24380c672375
Mark Nuzz <mark@bitnation.co>
2018-12-07 11:04:23 -0700

    Minor correction to script

--- _publish/postinstall.sh
+++ _publish/postinstall.sh
@@ -3,9 +3,10 @@
 # fail if any commands fails
 set -e
 set -x
-pwd
+
 PACKAGE_VERSION=$1
 
 echo "Downloading Panthalassa Binary Release ${PACKAGE_VERSION}"
 curl -L "https://github.com/Bit-Nation/panthalassa/releases/download/${PACKAGE_VERSION}/panthalassa-binaries-${PACKAGE_VERSION}.zip" -o panthalassa.zip
 unzip panthalassa.zip
+rm panthalassa.zip
