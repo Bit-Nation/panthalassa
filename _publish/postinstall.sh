0e066e39e20948e667d8b89b66021afcef23d59a
Mark Nuzz <mark@bitnation.co>
2018-12-07 08:48:44 -0700

    Work in progress

--- _publish/postinstall.sh
+++ _publish/postinstall.sh
@@ -2,9 +2,10 @@
 
 # fail if any commands fails
 set -e
-
-PACKAGE_VERSION=$(node -p "require('./package.json').version")
+set -x
+pwd
+PACKAGE_VERSION=$1
 
 echo "Downloading Panthalassa Binary Release ${PACKAGE_VERSION}"
-curl https://github.com/Bit-Nation/panthalassa/releases/download/$(PACKAGE_VERSION)/panthalassa-binaries-$(PACKAGE_VERSION).zip -o panthalassa.zip
-unzip build.zip
+curl "https://github.com/Bit-Nation/panthalassa/releases/download/${PACKAGE_VERSION}/panthalassa-binaries-${PACKAGE_VERSION}.zip" -o panthalassa.zip
+unzip panthalassa.zip
